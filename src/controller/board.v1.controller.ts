6ff85c9a609284af7610b562c6b493c76fd4902e
startergate <startergate36@gmail.com>
2020-03-19 22:22:08 +0900

    [CREATE] 게시물 생성 데이터 인증용 Joi

--- src/controller/board.v1.controller.ts
+++ src/controller/board.v1.controller.ts
@@ -1,13 +1,25 @@
 import { Context } from 'koa';
 import * as Joi from '@hapi/joi';
 
-export const getPost = (ctx: Context) => {
-
-};
+export const getPost = (ctx: Context) => {};
 
 export const getPosts = (ctx: Context) => {};
 
-export const postPost = (ctx: Context) => {};
+export const postPost = (ctx: Context) => {
+  const NewPost = Joi.object().keys({
+    board_type: Joi.string()
+      .allow(['자유게시판', '공지사항'])
+      .required(),
+    title: Joi.string()
+      .min(1)
+      .max(255)
+      .required(),
+    content: Joi.string().required(),
+    is_anonymous: Joi.bool()
+  });
+
+  ctx.assert(NewPost.validate(ctx.request.body), 400);
+};
 
 export const putPost = (ctx: Context) => {};
 
