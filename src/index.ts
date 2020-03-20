1693dd917c59a5f0c9c6eda7d7d129c516769e02
startergate <startergate36@gmail.com>
2020-03-21 03:24:41 +0900

    [ETC] add logger

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import * as dotenv from 'dotenv';
 // import Koa.js related package
 import * as Koa from 'koa';
 import * as bodyParser from 'koa-bodyparser';
+import * as logger from 'koa-logger';
 
 import router from './router';
 import sessionCreator from './middlewares/session';
@@ -15,6 +16,7 @@ const app = new Koa();
 // setting port
 const port = process.env.PORT || 5000;
 
+app.use(logger());
 app.use(bodyParser());
 app.use(sessionCreator);
 app.use(router.routes()).use(router.allowedMethods());
