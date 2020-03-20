66c6aa7a6d51986ed19c0bc5e86fb302caddd317
startergate <startergate36@gmail.com>
2020-03-21 04:27:05 +0900

    [CREATE] test only index response

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,15 @@
 import * as Router from 'koa-router';
 
 import api from './api';
+import {Context} from "koa";
 
 const index = new Router();
 
 // 이 파일은 편집하지 마세요.
 index.use('/api', api.routes());
+
+index.all('/', (ctx: Context) => {
+  ctx.status = 400;
+});
 
 export default index;
