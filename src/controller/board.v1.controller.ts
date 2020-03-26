164ca7711d7b3a4dd212199cc0c462ab48b82210
startergate <startergate36@gmail.com>
2020-03-26 16:32:24 +0900

    [UPDATE] 회의 사항 반영

--- src/controller/board.v1.controller.ts
+++ src/controller/board.v1.controller.ts
@@ -12,17 +12,21 @@ const PostIdInParam = Joi.object().keys({
 export const getPost = async (ctx: Context) => {
   ctx.assert(PostIdInParam.validate(ctx.params), 400);
 
-  ctx.body = await post.findByPk(ctx.params.postid);
+  ctx.body = {
+    data: await post.findByPk(ctx.params.postid)
+  };
 };
 
 export const getPosts = async (ctx: Context) => {
-  ctx.body = await post.findAll({
-    offset: 20 * ctx.params.page || 0,
-    limit: 20
-  });
+  ctx.body = {
+    data: await post.findAll({
+      offset: 20 * ctx.params.page || 0,
+      limit: 20
+    })
+  };
 };
 
-export const postPost = (ctx: Context) => {
+export const postPost = async (ctx: Context) => {
   const NewPost = Joi.object().keys({
     board_type: Joi.string()
       .allow(['자유게시판', '공지사항'])
@@ -35,17 +39,17 @@ export const postPost = (ctx: Context) => {
     is_anonymous: Joi.bool()
   });
 
-  ctx.assert(NewPost.validate(ctx.request.body), 400);
+  ctx.assert(!NewPost.validate(ctx.request.body).error, 400);
 
   const body = ctx.request.body;
   body.user_id = ctx.user.id;
 
-  post.create(body);
+  await post.create(body);
 
   ctx.status = 201;
 };
 
-export const putPost = (ctx: Context) => {
+export const putPost = async (ctx: Context) => {
   const OldPost = Joi.object().keys({
     board_type: Joi.string().allow(['자유게시판', '공지사항']),
     title: Joi.string()
@@ -57,22 +61,24 @@ export const putPost = (ctx: Context) => {
 
   // TODO: 회원 권한 검사 (본인)
 
-  ctx.assert(PostIdInParam.validate(ctx.params), 400);
-  ctx.assert(OldPost.validate(ctx.request.body), 400);
+  ctx.assert(!PostIdInParam.validate(ctx.params).error, 400);
+  ctx.assert(!OldPost.validate(ctx.request.body).error, 400);
 
-  post.update(ctx.request.body, {
+  ctx.assert(!(await post.update(ctx.request.body, {
     where: {
       post_id: ctx.params.postid
     }
-  });
+  })), 404);
+
+  ctx.status = 200;
 };
 
-export const deletePost = (ctx: Context) => {
+export const deletePost = async (ctx: Context) => {
   ctx.assert(PostIdInParam.validate(ctx.params), 400);
 
   // TODO: 회원 권한 검사 (본인 or 관리자)
 
-  post.destroy(ctx.params.postid);
+  ctx.assert(!(await post.destroy(ctx.params.postid)), 404);
 
   ctx.status = 200;
 };
