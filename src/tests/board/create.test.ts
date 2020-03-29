a0d2deac1e9d53377f0df8ab5d11d7427cac4251
startergate <startergate36@gmail.com>
2020-03-29 14:31:23 +0900

    [ETC] 게시판 테스트 - 데이터 없이 생성

--- src/tests/board/create.test.ts
+++ src/tests/board/create.test.ts
@@ -1,8 +1,15 @@
+import { Map } from 'immutable';
 import * as request from 'supertest';
 import app from '../../';
 
+const api = '/api/board/v1/';
+
+const authKey = '';
+
 test('create without body', async () => {
-  const response = await request(app.callback()).get('/');
-  expect(response.status).toBe(403);
-  expect(response.text).toBe('Forbidden');
+  const response = await request(app.callback())
+    .post(api)
+    .set('Authorization', authKey);
+  expect(response.status).toBe(400);
+});
 });
