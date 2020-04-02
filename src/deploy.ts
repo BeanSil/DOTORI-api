4cf9e23e8fb5e3bc34613c8c75227783674c430b
startergate <startergate36@gmail.com>
2020-04-02 16:41:58 +0900

    [UPDATE] 환경 변수 로딩 순서 변경

--- src/deploy.ts
+++ src/deploy.ts
@@ -1,16 +1,17 @@
 // 이 파일은 코드가 서버로써 실행되어야 할 때 실행됩니다.
 
-// koa 관련 스크립트를 실행하기 전에 환경 변수를 로드해야 합니다. 이에, app은 .env 설정 후에 import 합니다.
+// koa 관련 스크립트를 실행하기 전에 환경 변수를 로드해야 합니다. 이에, app은 .env.deploy 설정 후에 import 합니다.
 /* eslint-disable import/first */
 import * as path from 'path';
 
 import * as dotenv from 'dotenv';
 
-// 기존의 .env 파일은 글로벌 설정으로 사용할 수 있습니다.
-dotenv.config();
+// 각 환경에서만 필요한 변수를 가져옵니다.
+dotenv.config({ path: path.join(__dirname, `../.env.${process.env.NODE_ENV}`) });
 
-// 각 실행 환경에 따라 바뀌는 설정은 .env.test 형식의 파일에 넣어주세요.
-dotenv.config({ path: path.join(__dirname, `'/.env.${process.env.NODE_ENV}`) });
+// 모든 환경에서 사용되는 변수를 불러옵니다.
+// override는 되지 않습니다.
+dotenv.config();
 
 import app from './';
 /* eslint-enable import/first */
--- src/tests/index.ts
+++ src/tests/index.ts
@@ -1,8 +1,8 @@
 // 이 스크립트는 모든 테스트 전에 실행됩니다.
-// DB 백업이나, .env 설정 등의 역할을 합니다.
+// DB 백업이나, 환경 변수 설정 등의 역할을 합니다.
 
 import * as dotenv from 'dotenv';
 
 import * as path from 'path';
-dotenv.config();
 dotenv.config({ path: path.join(__dirname, `'/.env.test`) });
+dotenv.config();
