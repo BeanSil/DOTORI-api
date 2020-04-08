abfd0a5d3c9400ea4854e72cf4dbeb06dad263ba
hmoon630 <s19017@gsm.hs.kr>
2020-04-08 13:13:49 +0900

    [FIX] Model Export

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,12 +1,14 @@
 import { Sequelize } from 'sequelize';
 import { UserFactory } from './User';
 import { db, userDb } from './setting';
+import { LaptopArchiveFactory } from './LaptopArchive';
 
 const sequelize = new Sequelize(db);
 
 const sequelizeUser = new Sequelize(userDb);
 
 const user = UserFactory(sequelizeUser);
+const laptop = LaptopArchiveFactory(sequelize);
 
 const sync = sequelize.createSchema(db.database, {}).then(() => {
   return sequelize.sync();
@@ -18,4 +20,4 @@ const syncUser = sequelizeUser.createSchema(userDb.database, {}).then(() => {
 
 const waitForSync = Promise.all([sync, syncUser]);
 
-export { sequelize, Sequelize, waitForSync, user };
+export { sequelize, Sequelize, waitForSync, user, laptop };
