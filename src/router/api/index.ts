90ba8e1a56519fdf8f825c6765c6b5f618fb9e47
startergate <startergate36@gmail.com>
2020-04-23 08:10:01 +0900

    [ADD] 테스트용 API

--- src/router/api/index.ts
+++ src/router/api/index.ts
@@ -2,10 +2,15 @@ import * as Router from 'koa-router';
 
 import board from './board';
 import score from './score';
+import { Context } from 'koa';
 
 const api = new Router();
 
 api.use('/board', board.routes());
 api.use('/score', score.routes());
+
+api.get('/', (ctx: Context) => {
+  ctx.status = 200;
+});
 
 export default api;
