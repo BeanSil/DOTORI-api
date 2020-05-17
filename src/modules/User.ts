ed23d8725619c18156919341e5a3521c737e9c43
startergate <startergate36@gmail.com>
2020-05-17 17:47:19 +0900

    [ADD] User authority
    #50 반영 @unperknown

--- src/modules/User.ts
+++ src/modules/User.ts
@@ -9,8 +9,16 @@ export abstract class AbstractUser {
   readonly suspended_until: Date;
   readonly created_at: Date;
 
-  constructor(json: string = '') {
+  protected constructor(json: string = '') {
     if (json) Object.assign(this, JSON.parse(json));
+  }
+
+  authority(): Authority {
+    if (this.grade > 0) return Authority.STUDENT;
+    else if (this.class === 0) return Authority.PARENT;
+    else if (this.class === 1) return Authority.TEACHER;
+    else if (this.class === 2) return Authority.DOMITORY;
+    else return Authority.ANONYMOUS;
   }
 }
 
@@ -24,4 +32,17 @@ export class AnonymousUser extends AbstractUser {
   constructor() {
     super();
   }
+
+  authority(): Authority {
+    return Authority.ANONYMOUS;
+  }
+}
+
+export enum Authority {
+  STUDENT,
+  PARENT,
+  TEACHER,
+  DOMITORY,
+  ANONYMOUS
+
 }
