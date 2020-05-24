aa17c52fa58d6bfab332a37fe8a60a79898feb99
startergate <startergate36@gmail.com>
2020-05-24 22:03:47 +0900

    [ADD] login api

--- src/controller/account.v1.controller.ts
+++ src/controller/account.v1.controller.ts
@@ -1,6 +1,42 @@
 import { Context } from 'koa';
-import * as Joi from '@hapi/joi';
+
+import * as crypto from 'crypto';
+
+import * as redis from 'redis';
+import * as uuid from 'uuid';
+import * as Joi from "@hapi/joi";
+import { user } from "../models";
+
+const client = redis.createClient();
+const hash = crypto.createHash('sha512');
 
 export const getUserBySession = (ctx: Context) => {
-  ctx.body = ctx.user
+  delete ctx.user.pid;
+  delete ctx.user.pw;
+  ctx.body = { data: ctx.user }
 };
+
+export const createSession = async (ctx: Context) => {
+  const loginData = Joi.object({
+    id: Joi.string().required(),
+    password: Joi.string().required()
+  });
+
+  ctx.assert(!loginData.validate(ctx.body).error, 400);
+
+  let data = ctx.body;
+  hash.update(data.password);
+  data.password = hash.digest('hex');
+
+  const result = await user.findOne({ where: data, attributes: ['pid'] });
+
+  ctx.assert(result, 400);
+
+  const session = uuid.v4();
+
+  if (!(await client.set(session, result.pid.toString()))) {
+    ctx.throw(500);
+  }
+
+  ctx.body = { data: session }
+}
--- src/router/api/account/v1/index.ts
+++ src/router/api/account/v1/index.ts
@@ -1,9 +1,10 @@
 import * as Router from 'koa-router';
-import { getUserBySession } from "../../../../controller/account.v1.controller";
-import { LoginRequired } from "../../../../utils/authorization";
+import {createSession, getUserBySession} from "../../../../controller/account.v1.controller";
+import {LoginRequired, MustNotLoggedIn} from "../../../../utils/authorization";
 
 const v1 = new Router();
 
-v1.get('/session', LoginRequired, getUserBySession)
+v1.get('/session', LoginRequired, getUserBySession);
+v1.put('/session', MustNotLoggedIn, createSession);
 
 export default v1;
