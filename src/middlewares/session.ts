d3c63f54328f5603e00c6ec3751042fe6caf8e7a
startergate <startergate36@gmail.com>
2020-05-25 07:59:41 +0900

    [ETC] yarn pretty

--- src/middlewares/session.ts
+++ src/middlewares/session.ts
@@ -9,7 +9,8 @@ const client = redis.createClient();
 
 const sessionCreator = async (ctx: Context, next: Next) => {
   if (ctx.request.headers.authorization) {
-    client.get(ctx.request.headers.authorization, async (err, pid) => {  // redis 4 나오면 await으로 변경
+    client.get(ctx.request.headers.authorization, async (err, pid) => {
+      // redis 4 나오면 await으로 변경
       if (err) ctx.throw(500);
       ctx.user = pid ? new User(await user.findByPk(pid)) : AnonymousUser;
       await next();
