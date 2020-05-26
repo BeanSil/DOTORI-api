48f00d2cbac969699871bf88387d169f87167d66
Choi-YeonJin <41119288+Choi-YeonJin@users.noreply.github.com>
2020-05-26 19:35:34 +0900

    [FIX] router error 수정

--- src/router/api/account/v1/index.ts
+++ src/router/api/account/v1/index.ts
@@ -14,8 +14,8 @@ const v1 = new Router();
 v1.get('/session', LoginRequired, getUserBySession);
 v1.put('/session', MustNotLoggedIn, createSession);
 v1.delete('/session', LoginRequired, deleteSession); // 로그아웃
-v1.put('/user', MustNotLoggedIn, createUser); // 회원가입
-v1.post('/user', LoginRequired, modifyUser); // 회원 정보 수정
+v1.post('/user', MustNotLoggedIn, createUser); // 회원가입
+v1.put('/user', LoginRequired, modifyUser); // 회원 정보 수정
 v1.delete('/user', MustNotLoggedIn, deleteUser); // 회원 탈퇴
 
 export default v1;
