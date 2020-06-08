d4db4ec98d961d70bb92232c5dd549328ff6c2e0
startergate <startergate36@gmail.com>
2020-06-08 16:51:47 +0900

    [ADD] 권한 제외한 API

--- src/controller/dormitory.v1.controller.ts
+++ src/controller/dormitory.v1.controller.ts
@@ -1,5 +1,41 @@
 import {Context} from "koa";
-import {scoreArchive} from "../models";
+import {laptop, music, scoreArchive} from "../models";
 
 export const getSummary = async (ctx: Context) => {
+  const user = ctx.user;
+
+  const [ score, laptopRecord, musics ] = await Promise.all([
+    scoreArchive.findAll({
+      where: {
+        user_id: user.pid
+      }
+    }),
+    laptop.findOne({
+      where: {
+        user_id: ctx.user.pid,
+        createdAt: new Date().toISOString().slice(0, 10)
+      }
+    }),
+    music.findAll({
+      where: {
+        user_id: ctx.user.pid,
+        createdAt: new Date().toISOString().slice(0, 10)
+      }
+    }),
+  ]);
+
+  const scores = score.map(
+    /* istanbul ignore next */ (archive: any) => archive.score
+  );
+
+  ctx.body = {
+    data: {
+      score: scores,
+      laptop: {
+        room: laptopRecord.room,
+        seat: laptopRecord.seat
+      },
+      music: musics,
+    }
+  };
 };
